d6f0e39f022d4ca94208254d06f54f6b8ae18256
BoiluTheGod <arthur.poullain@gmail.com>
2021-04-27 16:28:59 +0200

    P2_02 Update and recursive function training

--- P2_02_categ_scraper.py
+++ P2_02_categ_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
+
 # Analyze a book's product page
 def book_analyzer(target_url):
 	response = requests.get(target_url)
@@ -10,7 +11,6 @@ def book_analyzer(target_url):
 		soup = BS(response.text, 'lxml')
 		book['product_page_url'] = response.url
 		book['title'] = soup.find('h1').text
-		# Does it have a description ?
 		if soup.find('div', {'id': 'product_description'}) is None:
 			book['product_description'] = 'No desc available'
 		else:
@@ -30,66 +30,63 @@ def book_analyzer(target_url):
 		book['image_url'] = soup.findAll('img')[0]['src'].replace('../../', 'http://books.toscrape.com/')
 
 		# Dictionnary to loop, compare & translate the website indexes we need
-		specs_to_keep = {'UPC': 'universal_product_code', 'Product Type': False,
-						 'Price (incl. tax)': 'price_including_tax', 'Price (excl. tax)': 'price_excluding_tax',
-						 'Tax': False, 'Availability': 'number_available', 'Number of Reviews': False}
+		specs_to_keep = {
+			'UPC': 'universal_product_code',
+			'Price (incl. tax)': 'price_including_tax', 'Price (excl. tax)': 'price_excluding_tax',
+			'Availability': 'number_available'
+			}
+		desc_specs = soup.find('table', {'class': 'table table-striped'}).findAll('th')
+		desc_values = soup.find('table', {'class': 'table table-striped'}).findAll('td')
+		for i in range(0, len(desc_specs)):
+			if desc_specs[i].text in specs_to_keep:
+				book[specs_to_keep[desc_specs[i].text]] = desc_values[i].text
 
-		desc_specs = soup.find('table', {'class': 'table table-striped'}).findAll('td')
-		index = 0
-		for spec in specs_to_keep:
-			if specs_to_keep[spec] is not False:
-				# print(specs_to_keep[spec] + ' = ' + desc_specs[index].text)
-				book[specs_to_keep[spec]] = desc_specs[index].text
-			index += 1
-		# Translation of weird symbol before '£'
 		book['price_including_tax'] = book['price_including_tax'].replace('Â', '')
 		book['price_excluding_tax'] = book['price_excluding_tax'].replace('Â', '')
-		# Returns the book dict with every scraped info -- WORKING
+		# Making the function return the book dict so I can retrieve it later on
 		return book
-#def csv_magic(category_books):
-#	print(category_books)
 
-def find_all_books_from_category(pages_to_analyze):
+# Sends every book from the defined page to the analyzer and returns the infos
+def send_books_from_page_to_analyzer(link):
 	booksfromcategory = []
-	print('We will analyze' , pages_to_analyze)
-	linkBegin = 'http://books.toscrape.com/catalogue/'
-	for link in pages_to_analyze:
-		print(' Currently Analyzing : ' + link)
-		response = requests.get(link)
-		if response.ok:
-			soup = BS(response.text, 'lxml')
-			product_pages_finder= soup.findAll('h3')
-			for product_page in product_pages_finder:
-				product_page_link = linkBegin + product_page.find('a')['href'].replace('../../../', '')
-				booksfromcategory.append(book_analyzer(product_page_link))
-	print('This return is working properly')
+	base_link = 'http://books.toscrape.com/catalogue/'
+	print(' Currently Analyzing : ' + link)
+	response = requests.get(link)
+	if response.ok:
+		soup = BS(response.text, 'lxml')
+		product_pages_finder = soup.findAll('h3')
+		for product_page in product_pages_finder:
+			product_page_link = base_link + product_page.find('a')['href'].replace('../../../', '')
+			booksfromcategory.append(book_analyzer(product_page_link))
 	# Returns a dictionnary of the category containing every needed book as subdictionnaries -- WORKING
 	return booksfromcategory
 
 
-# Counts pages and call find_all_books_from_category when done
-def count_pages(url,pagesurl):
+def scan_all_books_from_page(mypage):
+	books_from_page = send_books_from_page_to_analyzer(mypage)
+	return books_from_page
+
+
+# Analyze our page and goes to the next one if it featyres a next button
+def send_page_to_scraper_and_try_to_find_next(url, recursive_booklist):
+	booklist = recursive_booklist
 	response = requests.get(url)
-	pagesurl.append(url)
 	if response.ok:
 		soup = BS(response.text, 'lxml')
-		# Checks if a next button exists & re-calls itself if so
+		recursive_booklist.append(scan_all_books_from_page(url))
+		# Si il y a un bouton :
 		if soup.find('li', {'class': 'next'}) is not None:
 			nextpagebutton = soup.find('li', {'class': 'next'}).find('a')['href']
 			newurl = url.replace('index.html', nextpagebutton)
-			count_pages(newurl,pagesurl)
-		else:
-			print(str(len(pagesurl)) + ' page(s) successfully found.')
-			books_dictionnary = find_all_books_from_category(pagesurl)
-			# If we print book_dictionnary here, we can see it is perfect, but I can't return it for some reason,
-			# Whenever I try to get the value returned by this function, I do get a None Type.
-			# But printing it here works fine and delivers it the way it should.
-			return books_dictionnary
+			send_page_to_scraper_and_try_to_find_next(newurl, booklist)
+		return recursive_booklist
 
 
 def main():
-	pagesurl = []
-	all_books = count_pages('http://books.toscrape.com/catalogue/category/books/romance_8/index.html', pagesurl)
+	# Initializing a book list to use as parameter for recursivity
+	recursive_booklist = []
+	all_books = send_page_to_scraper_and_try_to_find_next(
+		'http://books.toscrape.com/catalogue/category/books/romance_8/index.html', recursive_booklist)
 	print(all_books)
 
 
